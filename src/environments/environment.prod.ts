43d17ea9f82db4192f57364269407bf399a0a047
rajatgouri <rajatgouri020@gmail.com>
2020-05-29 19:55:32 +0530

    add IP address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_api_uri: 'https://api.ventinghub.com/',
   base_ws_uri: 'ws://api.ventinghub.com/',
-  socket_url: 'https://178.128.144.62/'
+  socket_url: 'http://178.128.144.62/'
 };
