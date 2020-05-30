df4102293f2413d1c07630539693af0381ff34df
Rajat Gouri <49143197+rajatgouri@users.noreply.github.com>
2020-05-30 09:49:10 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_api_uri: 'https://api.ventinghub.com/',
   base_ws_uri: 'ws://api.ventinghub.com/',
-  socket_url: 'http://178.128.144.62:3000/'
+  socket_url: 'http://ventinghub.com/'
 };
