aa8e6d7c14bdccc3526a3b4aae7b7ee39cdb0489
rajatgouri <rajatgouri020@gmail.com>
2020-05-30 21:14:11 +0530

    changes made in dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_api_uri: 'https://api.ventinghub.com/',
   base_ws_uri: 'ws://api.ventinghub.com/',
-  socket_url: 'http://ventinghub.com/'
+  socket_url: 'https://ventinghub.com/'
 };
